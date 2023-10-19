96700bc968ab2ab8e6837c9e890fd4eba05a01a6
Jpngeta <jpngeta@gmail.com>
2023-10-19 15:51:16 +0300

    _execve file

--- _exec.c
+++ _exec.c
@@ -3,9 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * _exec - Execute a command.
+ * @cmd: The command to execute.
+ * @argv: An array of arguments for the command.
+ * @env: The environment variables.
+ */
 void _exec(char *cmd, char **argv, char **env)
 {
 	execve(cmd, argv, env);
 	perror("execve");
 	exit(1);
 }
+
